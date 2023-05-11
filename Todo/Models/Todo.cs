43ccf177a35d4c9834f32e402693bcc6ebe93473
Emerson Costa de Oliveira <emerson.costa@localiza.com>
2023-05-10 21:33:48 -0300

    Inicio

---
+++ Todo/Models/Todo.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Todo.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
